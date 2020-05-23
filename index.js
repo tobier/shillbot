7f27aa40b51368315c20979ce446b80333087e56
Tobias Eriksson <tti.eriksson@gmail.com>
2020-05-23 16:16:08 +0200

    Add basic "hello world" bot

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const config = require('./config.json');
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(config.discord.token);
